33cf0268993aa871ef06ffce7acc7c70c5d0355e
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-19 21:24:31 +0900

    ボタンのホバーを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import MySkills from './pages/MySkills'
+import "./App.css";
 
 function App() {
   return (
--- frontend/src/components/ShowOneSkillButton.tsx
+++ frontend/src/components/ShowOneSkillButton.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { ShowOneSkillButtonProps } from "../types/showOneSkillButton";
 
 const ShowOneSkillButton:React.FC<{skillData:ShowOneSkillButtonProps}>=({skillData})=>{
+    const [isHover,setIsHover]=useState<boolean>(false);
     return(
         <div>
-            <button style={{color:skillData.color}}>{skillData.skill}</button>
+            <button className={`ShowOneSkillButton inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide transition-colors duration-100 rounded-md bg-white border-2`} style={isHover?{color:"white", backgroundColor:skillData.color, borderColor:skillData.color}:{ color:skillData.color, borderColor:skillData.color}} onMouseEnter={()=>{setIsHover(true)}} onMouseLeave={()=>{setIsHover(false)}}>{skillData.skill}</button>
         </div>
     )
 };
