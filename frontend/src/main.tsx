dad92355ee60102127f6770abf377b133cf445c1
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-20 14:47:58 +0900

    ルータを追加

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import MySkills from './pages/MySkills.tsx'
 import './index.css'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />
+  },
+  {
+    path:"/my-skill",
+    element:<MySkills />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Home:React.FC=()=>{
+    return(
+        <div>
+            <p>Home!</p>
+        </div>
+    )
+};
+export default Home;
