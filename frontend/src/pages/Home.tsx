6b38045abd302ed49b171baf27828e9acbe47a26
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-20 15:31:43 +0900

    フッターを追加

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,8 @@ import React from "react"
 const Home:React.FC=()=>{
     return(
         <div>
-            <header className="sticky top-0">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <header className="sticky top-0 bg-white z-10 shadow">
+                <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
                     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -13,7 +13,7 @@ const Home:React.FC=()=>{
                         {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
                         <path fill="currentColor" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z"/>
                     </svg>
-                        <span className="ml-3 text-xl">Skill Mapper</span>
+                        <span className="ml-3 text-2xl">Skill Mapper</span>
                     </a>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <a className="mr-5 hover:text-gray-900">First Link</a>
@@ -28,6 +28,17 @@ const Home:React.FC=()=>{
                     </button>
                 </div>
             </header>
+            <div id="main" className="min-h-screen relative">
+                <p>main</p>
+                <footer className="bg-gray-100 absolute bottom-0 w-full">
+                    <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                        <p className="text-gray-500 text-sm text-center sm:text-left">Made with ❤️ and 
+                            <a href="https://github.com/krn-7717/over-halolle" className="text-gray-600 ml-1" target="_blank" rel="noopener noreferrer">@over-halolle</a>
+                        </p>
+                        <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-500 text-sm">Enjoy the journey of acquiring technical skills!</span>
+                    </div>
+                </footer>
+            </div>
         </div>
     )
 };
