826e97c1a106b44b2e647f15be4e9b3de68f710d
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-20 15:07:48 +0900

    ヘッダーを追加

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,31 @@ import React from "react"
 const Home:React.FC=()=>{
     return(
         <div>
-            <p>Home!</p>
+            <header className="sticky top-0">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg> */}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-14 h-14 text-blue-500 p-2" >
+                        {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                        <path fill="currentColor" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z"/>
+                    </svg>
+                        <span className="ml-3 text-xl">Skill Mapper</span>
+                    </a>
+                    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <a className="mr-5 hover:text-gray-900">First Link</a>
+                    <a className="mr-5 hover:text-gray-900">Second Link</a>
+                    <a className="mr-5 hover:text-gray-900">Third Link</a>
+                    <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                    </nav>
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                    </button>
+                </div>
+            </header>
         </div>
     )
 };
