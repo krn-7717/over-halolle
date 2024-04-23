22c3700e00176e5a4529ad83559fa6c95b9eaaff
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-23 15:12:58 +0900

    ログアウトボタンを追加

--- frontend/src/pages/MainLayout.tsx
+++ frontend/src/pages/MainLayout.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const MainLayout:React.FC=()=>{
+    const handleClickLogout=():void=>{
+        localStorage.clear()
+    }
     return(
         <div>
             <header className="sticky top-0 bg-white z-10 shadow">
@@ -14,11 +17,12 @@ const MainLayout:React.FC=()=>{
                         <span className="ml-3 text-xl md:text-3xl">Skill Mapper</span>
                     </a>
                     <div>
-                        <Link to={"/login"} className="inline-flex justify-center items-center m-2 border-0 py-1 px-3 w-32 focus:outline-none rounded text-base text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-30">
-                            ログイン
-                        </Link>
-                        <Link to={"/signup"} className="inline-flex justify-center items-center m-2 border-0 py-1 px-3 w-32 focus:outline-none rounded text-base text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-30">
-                            サインアップ
+                        <Link to={"/"} onClick={handleClickLogout} className="inline-flex justify-center items-center m-2 border-0 py-1 px-3 w-32 focus:outline-none rounded text-base text-gray-600 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:ring-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-[0.8rem] h-[0.8rem] text-gray-600 me-2">
+                                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
+                            </svg>
+                            ログアウト
                         </Link>
                     </div>
                 </div>
