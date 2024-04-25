e6b288e78d10ad6cb746df9df7c07bacbb112cfe
yuusuke <xiangzhongyoufu@gmail.com>
2024-04-26 03:42:33 +0900

    []

--- backend/controllers/skill_controller.py
+++ backend/controllers/skill_controller.py
@@ -1,8 +1,15 @@
-from flask import Blueprint
 from backend.common.models.skill import Skill
+from flask import Blueprint
+from flask import jsonify
+from backend import db
 
 bp = Blueprint("skills", __name__, url_prefix="/skills")
 
-@bp.route("/")
+@bp.route("/", methods=["GET"])
 def index():
-    return "Hello skill page"
+    skills = Skill.query.filter(Skill.id == 1).all()
+    return jsonify({
+        "id": skills[0].id, 
+        "skill": skills[0].skill, 
+        "color": skills[0].color
+    })
