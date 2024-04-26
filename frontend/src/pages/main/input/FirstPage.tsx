4aa79a33fe367270e1b4650a738018323ef97657
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-26 16:44:37 +0900

    初期値を修正

--- frontend/src/pages/main/input/FirstPage.tsx
+++ frontend/src/pages/main/input/FirstPage.tsx
@@ -24,7 +24,7 @@ const FirstPage:React.FC<{skillDefault:string|undefined,selectOptins:SelectOptio
             <h2 className="py-2">理解度</h2>
             <div className="relative mb-12">
                 <label htmlFor="understandingLevel" className="sr-only">Labels range</label>
-                <input id="understandingLevel" value={UnderstangingDefault} onChange={handleChangeUnderstanding} name="understandingLevel" type="range" defaultValue={0} min={0} max={100} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" />
+                <input id="understandingLevel" value={UnderstangingDefault} onChange={handleChangeUnderstanding} name="understandingLevel" type="range" min={0} max={100} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" />
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">min</span>
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/2 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6"></span>
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">max</span>
@@ -32,7 +32,7 @@ const FirstPage:React.FC<{skillDefault:string|undefined,selectOptins:SelectOptio
             <h2 className="py-2">自信度</h2>
             <div className="relative mb-12">
                 <label htmlFor="confidenceLevel" className="sr-only">Labels range</label>
-                <input id="confidenceLevel" value={confidenceDefault} onChange={handleChangeConfidence} name="confidenceLevel" type="range" defaultValue={0} min={0} max={100} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" />
+                <input id="confidenceLevel" value={confidenceDefault} onChange={handleChangeConfidence} name="confidenceLevel" type="range" min={0} max={100} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" />
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">min</span>
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/2 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6"></span>
                 <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">max</span>
