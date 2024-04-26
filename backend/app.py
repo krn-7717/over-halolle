81f206ab399203b024bdf7dff01986f15ce2e792
yuusuke <xiangzhongyoufu@gmail.com>
2024-04-27 00:01:14 +0900

    [add] CORS設定の完了

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from backend import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(
+    app,
+    CORS_ALLOW_HEADERS="*",
+    CORS_ORIGIN="*",
+    CORS_METHODS=["GET", "HEAD", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+    CORS_SUPPORTS_CREDENTIALS=True
+)
 
 if __name__ == "__main__":
     app.run()
