f3fea47f3ea063786671252940d06c5cf51b7f15
yuusuke <xiangzhongyoufu@gmail.com>
2024-04-28 00:53:30 +0900

    [add] ユーザ名変更機能の追加

--- frontend/src/api/settings/userNameApi.ts
+++ frontend/src/api/settings/userNameApi.ts
@@ -4,10 +4,17 @@ const BACKEND_URL_DUMMY=import.meta.env.VITE_BACKEND_URL_DUMMY;
 export const saveUserName=async(userId:number,newUserName:string):Promise<undefined>=>{
     const postData={userId:userId,newUserName:newUserName};
     console.log("<username api (saveUserName)> POST : ",postData);
-    const response= await fetch(BACKEND_URL_DUMMY);
+    const response = await fetch(`${BACKEND_URL}/users/${postData.userId}`, {
+        method: "POST",
+        body: JSON.stringify(postData), 
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
     if(!response.ok){
         throw new Error("ユーザー名を変更できません");
     };
     const data=response.json(); 
     console.log("<username api (saveUserName)> Response : なし");
+    console.log(data)
 };
