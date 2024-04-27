05f72fd1990f3782ac8f08609389595b4894aa79
ayakakawabe <kawabe.ayaka@icloud.com>
2024-04-27 22:28:08 +0900

    サインアップを本APIに置き換え

--- frontend/src/api/account/signupApi.ts
+++ frontend/src/api/account/signupApi.ts
@@ -1,24 +1,22 @@
 const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
-const BACKEND_URL_DUMMY=import.meta.env.VITE_BACKEND_URL_DUMMY;
 
-export const signup=async(email:string,password:string):Promise<{userId:number,userName:string}>=>{
+export const signup=async(email:string,password:string):Promise<{status:number,data:{id:number,name:string}}>=>{
     const postData={email:email,password:password};
     console.log("<signup api (signup)> POST : ",postData);
-    const response = await fetch(`${BACKEND_URL_DUMMY}/signup/`, {
+    const response = await fetch(`${BACKEND_URL}/signup/`, {
         method: "POST",
         body: JSON.stringify(postData), 
         headers: {
             "Content-Type": "application/json"
         }
     });
-    if(!response.ok){
-        if(response.status===409){
-            throw new Error("このメールアドレスはすでに使用されています");
-        }else{
-            throw new Error("サインアップ処理を完了できませんでした");
-        };
+
+    if(response.status!==409 && response.status!==200){
+        throw new Error("サインアップ処理を完了できませんでした");
     };
+
     const data=response.json();
-    console.log("<signup api (signup)> Response : ",{userId:1111,userName:"匿名ユーザー"});
-    return {userId:1111,userName:"匿名ユーザー"};
+    // console.log(data)
+    // console.log("<signup api (signup)> Response : ",{userId:1111,userName:"匿名ユーザー"});
+    return data;
 };
